0913c6038505d09f3336937da6e98e5a35fa54d5
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-04-28 15:47:59 +0530

    REFACRORED: Create Generic Class To Take Three Input

--- src/main/java/com/bl/demo/MaximumNumber.java
+++ src/main/java/com/bl/demo/MaximumNumber.java
@@ -1,13 +1,36 @@
 package com.bl.demo;
 
 public class MaximumNumber<T extends Comparable> {
+    T firstVariable;
+    T secondVariable;
+    T thirdVariable;
+    T[] array;
 
-    public T maximum(T[] array) {
+    public MaximumNumber(T firstVariable, T secondVariable, T thirdVariable) {
+        this.firstVariable = firstVariable;
+        this.secondVariable = secondVariable;
+        this.thirdVariable = thirdVariable;
+    }
+
+    public MaximumNumber(T[] array) {
+        this.array = array;
+    }
+
+    public T maximum() {
         T max = array[0];
-        for (int element=0 ; element<array.length ; element++) {
+        for (int element=0 ; element<array.length; element++) {
             if (array[element].compareTo(max) > 0)
                 max = array[element];
         }
         return max;
+    }
+
+    public T maximumValue() {
+        if (firstVariable.compareTo(secondVariable) > 0 && firstVariable.compareTo(thirdVariable) > 0)
+            return firstVariable;
+        else if (secondVariable.compareTo(thirdVariable) > 0)
+            return secondVariable;
+        else
+            return thirdVariable;
     }
 }
--- src/test/java/com/bl/demo/MaximumNumberTest.java
+++ src/test/java/com/bl/demo/MaximumNumberTest.java
@@ -7,19 +7,25 @@ public class MaximumNumberTest {
 
     @Test
     public void testCode_IntegerObjectArray_ShouldReturnMaximumNumber() {
-        Comparable max = new MaximumNumber<>().maximum( new Integer[] {1,2,3});
+        Comparable max = new MaximumNumber<>(new Integer[]{1, 2, 3}).maximum();
         Assert.assertEquals(3,max);
     }
 
     @Test
     public void testCode_FloatObjectArray_ShouldReturnMaximumNumber() {
-        Comparable max = new MaximumNumber<>().maximum( new Float[] {1.1f,2.1f,3.1f});
+        Comparable max = new MaximumNumber<>(new Float[] {1.1f,2.1f,3.1f}).maximum();
         Assert.assertEquals(3.1f, (Float) max,0.01);
     }
 
     @Test
-    public void testCode_StringObjectArray_ShouldReturnMaximumNumber() {
-        Comparable max = new MaximumNumber<>().maximum( new String[] {"abc","def","ghi"});
+    public void testCode_StringObjectArray_ShouldReturnMaximum() {
+        Comparable max = new MaximumNumber<>( new String[] {"abc","def","ghi"}).maximum();
         Assert.assertEquals("ghi",max);
+    }
+
+    @Test
+    public void testCodeConstructor_ObjectArray_ShouldReturnMaximum() {
+        Comparable max = new MaximumNumber<>(1,2,3).maximumValue();
+        Assert.assertEquals(3,max);
     }
 }
